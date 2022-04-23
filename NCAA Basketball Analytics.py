27afab5d414d1abe8dd28716d597b4c06273b5fd
adberrig <57606647+adberrig@users.noreply.github.com>
2022-04-23 16:06:40 -0400

    Update
    Added cache of statistics to speed up runtime. Added interactions with user in various forms.

--- NCAA Basketball Analytics.py
+++ NCAA Basketball Analytics.py
@@ -7,6 +7,7 @@ import time
 start_time = time.time()
 source_dir = os.path.dirname(__file__)
 full_path = os.path.join(source_dir, 'Testbase.db')
+new_path = os.path.join(source_dir, 'team_stats.json')
 conn = sqlite3.connect(full_path)
 c = conn.cursor()
 def create_table():
@@ -21,6 +22,8 @@ def create_table_name():
 def data_entry_name():
 	c.execute("INSERT INTO Names (Id, Name) VALUES(?, ?)", (Id, Name))
 	conn.commit()
+print("Welcome to NCAA Basketball Analytics!")
+print("Gathering up-to-date team info...")
 url = "https://api-basketball.p.rapidapi.com/teams"
 
 querystring = {"league":"116","season":"2021-2022"}
@@ -45,39 +48,89 @@ def read_data():
 	for i in data:
 		data2.append(i[0])
 read_data()
+data_set = []
+Question = input("Would you like to use cached data? y/n")
+if Question ==('n'):
+	count = 0
+	for i in data2:
+		url = "https://api-basketball.p.rapidapi.com/statistics"
+		querystring = {"season":"2021-2022","league":"116","team":"{}".format(i)}
 
+		headers = {
+			"X-RapidAPI-Host": "api-basketball.p.rapidapi.com",
+			"X-RapidAPI-Key": "4a9ae85127msh66e82c4bcc504b3p1dcd57jsn35a9ff4b2773"
+		}
 
-for i in data2:
-	url = "https://api-basketball.p.rapidapi.com/statistics"
-	querystring = {"season":"2021-2022","league":"116","team":"{}".format(i)}
+		response = requests.request("GET", url, headers=headers, params=querystring)
+		test = (json.loads(response.text))
+		data_set.append(test)
+		count = count + 1
+		if count == 1:
+			print("Gathering Up-to-Date Data! This process will take awhile.")
+		elif count == 68:
+			print("10% Complete")
+		elif count == 136:
+			print("20% Complete")
+		elif count == 204:
+			print("30% Complete")
+		elif count == 272:
+			print("40% Complete")
+		elif count == 340:
+			print("50% Complete")
+		elif count == 408:
+			print("60% Complete")
+		elif count == 476:
+			print("70% Complete")
+		elif count == 544:
+			print("80% Complete")
+		elif count == 612:
+			print("90% Complete")
+		value = test['response']
+		name1 = value['team']
+		name2 = name1['name']
+		val2 = (value['points'])
+		val3 = val2['for']
+		val4 = val3['average']
+		val5 = val2['against']
+		val6 = val5['average']
+		game = value['games']
+		win2 = game['wins']
+		win3 = win2['all']
+		win4 = win3['total']
+		loss2 = game['loses']
+		loss3 = loss2['all']
+		loss4 = loss3['total']
+		num2 = float(val4['all'])
+		num3 = float(val6['all'])
+		if win4 and loss4 != 0:
+			data_entry()
+	with open(new_path, 'w') as f:
+		json.dump(data_set, f, ensure_ascii=False, indent=4)
+elif Question ==('y'):
+	f = open(new_path)
+	xi = json.load(f)
+	print("This won't take long! Please wait a moment")
+	for i in xi:
+		value = i['response']
+		name1 = value['team']
+		name2 = name1['name']
+		val2 = (value['points'])
+		val3 = val2['for']
+		val4 = val3['average']
+		val5 = val2['against']
+		val6 = val5['average']
+		game = value['games']
+		win2 = game['wins']
+		win3 = win2['all']
+		win4 = win3['total']
+		loss2 = game['loses']
+		loss3 = loss2['all']
+		loss4 = loss3['total']
+		num2 = float(val4['all'])
+		num3 = float(val6['all'])
+		if win4 and loss4 != 0:
+			data_entry()
 
-	headers = {
-		"X-RapidAPI-Host": "api-basketball.p.rapidapi.com",
-		"X-RapidAPI-Key": "4a9ae85127msh66e82c4bcc504b3p1dcd57jsn35a9ff4b2773"
-	}
-
-	response = requests.request("GET", url, headers=headers, params=querystring)
-
-	test = (json.loads(response.text))
-	value = test['response']
-	name1 = value['team']
-	name2 = name1['name']
-	val2 = (value['points'])
-	val3 = val2['for']
-	val4 = val3['average']
-	val5 = val2['against']
-	val6 = val5['average']
-	game = value['games']
-	win2 = game['wins']
-	win3 = win2['all']
-	win4 = win3['total']
-	loss2 = game['loses']
-	loss3 = loss2['all']
-	loss4 = loss3['total']
-	num2 = float(val4['all'])
-	num3 = float(val6['all'])
-	if win4 and loss4 != 0:
-		data_entry()
 print("Done!")
 print("--- %s seconds ---" % (time.time() - start_time))
 c.close()
